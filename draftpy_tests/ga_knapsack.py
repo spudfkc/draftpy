2401a6e0d85499626ef700e66a66ede125678523
Nick Caley <nicholas.caley@vertical-knowledge.com>
2015-12-03 13:26:00 -0500

    Adding constraints around position, individual (lineup) length, min points, salary

--- draftpy_tests/ga_knapsack.py
+++ draftpy_tests/ga_knapsack.py
@@ -9,13 +9,15 @@ from deap import tools
 
 MAX_SALARY = 50000
 MAX_POINTS = 1000
+MIN_POINTS = 180
 INDV_INIT_SIZE = 8
 
-random.seed(64)
+random.seed()
 
 ## load players
 pd_players = pd.read_csv('DKSalaries.csv', header=0)
 players = [list(x) for x in pd_players.values]
+# print players
 
 num_players = len(players)
 
@@ -29,13 +31,26 @@ toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.att
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 def evalLineup(individual):
+    """lower is better?"""
+    positions = {}
     salary = 0.0
     points = 0.0
+    if len(individual) != 8:
+        return 10000, 0
     for item in individual:
         salary += float(players[item][2])
         points += float(players[item][4])
-    if len(individual) > MAX_POINTS or salary > MAX_SALARY:
+        pos = players[item][0]
+        pos_count = positions.get(pos, 0)
+        positions[pos] = pos_count + 1
+    if salary > MAX_SALARY:
         return 10000, 0
+    if points < MIN_POINTS:
+        return 10000, 0
+    # try to filter out more than x2 of same position
+    for k, v in positions.iteritems():
+        if v > 2:
+            return 10000, 0
     return salary, points
 
 def cxSet(ind1, ind2):
@@ -60,8 +75,8 @@ toolbox.register("mutate", mutSet)
 toolbox.register("select", tools.selNSGA2)
 
 def main():
-    random.seed(64)
-    NGEN = 500
+    random.seed()
+    NGEN = 3000
     MU = 50
     LAMBDA = 100
     CXPB = 0.7
@@ -72,11 +87,13 @@ def main():
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("avg", numpy.mean, axis=0)
     stats.register("std", numpy.std, axis=0)
-    stats.register("min", numpy.min, axis=0)
     stats.register("max", numpy.max, axis=0)
 
     algorithms.eaMuPlusLambda(pop, toolbox, MU, LAMBDA, CXPB, MUTPB, NGEN, stats, halloffame=hof)
     return pop, stats, hof
 
 if __name__ == "__main__":
-    main()
+    r = main()
+    print r
+    print ""
+    print [players[i] for i in r[0][0]]
